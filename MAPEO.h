5d09ae16fc9ffbf6ce6d01036c9716e4f7b61f66
C1prix <124991703+C1prix@users.noreply.github.com>
2023-08-09 15:51:44 -0600

    Create MAPEO.h

---
+++ MAPEO.h
@@ -0,0 +1,12 @@
+/* 
+ * File: Mapeo Voltaje
+ * Author: Gabriel Cipriano
+ * Comments:
+ * Revision history: 
+ */
+#ifndef MAPEO_H
+#define MAPEO_H
+
+float mapeo_variable(int valor);
+
+#endif // MAPEO_h
